25457fc73e87c1bdaec0f49ff94c382d4eceaa23
fclizhonglin <237067607@qq.com>
2017-08-06 22:40:48 +0800

    second commit

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    slug = models.CharField(max_length = 200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default = timezone.now)
+    
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __unicode__(self):
+        return self.title
--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
+# -*-coding:utf-8 -*-
 #!/usr/bin/env python
 import os
 import sys
 
 if __name__ == "__main__":
+    reload(sys)
+    sys.setdefaultencoding('utf-8') 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mblog.settings")
     try:
         from django.core.management import execute_from_command_line
